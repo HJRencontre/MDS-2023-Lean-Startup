3b281d805ff2cb37958ecea6ed4d0ad6917d0bb4
Hermann-Jude Rencontre <h.jrencontre@outlook.com>
2023-12-08 02:04:27 +0100

    Image fix

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -2,12 +2,12 @@ export default function Navbar() {
     return (
         <div className="navbar" id="home">
             <div className="flex-1">
-                <img src="src/assets/images/typography.png" className="w-24 md:w-40" />
+                <img src="../assets/images/typography.png" className="w-24 md:w-40" />
             </div>
             <div className="flex-none">
                 <a href="#bottom" className="border-2 btn btn-outline border-lime text-lime">
                     Je suis organisateur
-                    <img src="src/assets/forms/arrowdown.png" className="max-w-sm rounded-lg" />
+                    <img src="../assets/forms/arrowdown.png" className="max-w-sm rounded-lg" />
                 </a>
             </div>
         </div>
