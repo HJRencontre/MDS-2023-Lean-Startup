99b0a34fd4e9ffeab4a064bcf25c08777507410b
Hermann-Jude Rencontre <h.jrencontre@outlook.com>
2023-12-08 02:12:29 +0100

    Test image with external link

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ export default function Navbar() {
     return (
         <div className="navbar" id="home">
             <div className="flex-1">
-                <img src="../assets/images/typography.png" className="w-24 md:w-40" />
+                <img src="https://raw.githubusercontent.com/HJRencontre/MDS-2023-Lean-Startup/7bfe9ddd26904686f8d23103dcc191ac1ab12684/src/assets/images/typography.png" className="w-24 md:w-40" />
             </div>
             <div className="flex-none">
                 <a href="#bottom" className="border-2 btn btn-outline border-lime text-lime">
