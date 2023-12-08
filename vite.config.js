2f65647c90aed3c6522439a4d5c2bd22f903f809
Hermann-Jude Rencontre <h.jrencontre@outlook.com>
2023-12-08 01:59:03 +0100

    Fixes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/MDS-2023-lean-startup/",
+  base: "/MDS-2023-Lean-Startup/",
   plugins: [react()],
 })
